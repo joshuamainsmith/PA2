274ac45a71c28d32ace7b17c587e47b413914bb6
Josh <joshuamainsmith@protonmail.com>
2017-10-11 21:14:08 -0700

    Testing out the basics.

--- PA2/fileStream.cpp
+++ PA2/fileStream.cpp
@@ -1,9 +1,43 @@
+#include <fstream>
 #include <iostream>
-#include <iostream>
+
+using namespace std;
 
 int main ()
 {
+	/* Testing out the basics
+	 * Taken Directly from tutorialspoint.com 
+	 * Left for reference */
 
+	char data[100];
+
+	ofstream outfile;
+	outfile.open("somefile.dat");
+
+	cout << "Enter name: ";
+	cin.getline(data, 100);
+
+	outfile << data << endl;
+
+	cout << "Enter age: ";
+	cin >> data;
+	cin.ignore();
+
+	outfile << data << endl;
+
+	outfile.close();
+
+	ifstream infile;
+	infile.open("somefile.dat");
+
+	infile >> data;
+
+	cout << data << endl;
+
+	infile >> data;
+	cout << data << endl;
+
+	infile.close();
 
 	return 0;
 }
